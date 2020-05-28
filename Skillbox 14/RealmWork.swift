2512ddf0e61c216e71b760f0debc3994541738fe
Artem Akopian <artem7498@gmail.com>
2020-05-28 17:22:56 +1000

    first commit

---
+++ Skillbox 14/RealmWork.swift
@@ -0,0 +1,9 @@
+//
+//  RealmWork.swift
+//  Skillbox 14
+//
+//  Created by Артём on 5/27/20.
+//  Copyright © 2020 Artem A. All rights reserved.
+//
+
+import Foundation
---
+++ Skillbox 14/ToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoCell.swift
+//  Skillbox 14
+//
+//  Created by Артём on 5/27/20.
+//  Copyright © 2020 Artem A. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
